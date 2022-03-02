44414f828e49bdd17f299958cf92a129103e1747
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-02 17:43:57 +0000

    Swapping the inputs in the output

---
+++ Variables challenge.py
@@ -0,0 +1,19 @@
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a #if a=20 and b =3, then now, c=20
+a = b #here a=3(as a=b(which is 3))
+b = c #here b=20 as initially b was = 3
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
