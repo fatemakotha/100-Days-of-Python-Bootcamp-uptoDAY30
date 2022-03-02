e5f29aaac7a5fa6604de7f1dcd5999c09230d05d
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-02 17:58:50 +0000

    Band Name Generator Challenge

---
+++ Band Name Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+city_name = input("What's the name of the city you grew up in:")
+pet_name = input("What's the name of your pet:")
+
+print(city_name + " " + pet_name)
