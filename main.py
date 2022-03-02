28f9aa01542a46b2135498d7800f93e64d0d3f7b
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-02 15:54:52 +0000

    Introduced line gap

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 print('print("what to print")')
+
+print("Hello World!\nHellow World!\nHello World!")
