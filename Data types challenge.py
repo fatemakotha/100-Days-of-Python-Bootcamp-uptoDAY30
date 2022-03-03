b3a0d78ce76adb1b584a9434383d4e030ffe656f
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-03 17:20:18 +0000

    Split the input number of two digits and find their addition

---
+++ Data types challenge.py
@@ -0,0 +1,13 @@
+#Split the input number of two digits and find their addition
+#if your input number is 32, the output should be 3+2, which is 5
+number = input("Type a two digit number")
+print(type(number))
+
+first_digit = number[0]
+second_digit = number[1]
+
+conv_first_digit = int(first_digit)
+conv_second_digit = int(second_digit)
+
+addition = conv_first_digit + conv_second_digit
+print(addition)
