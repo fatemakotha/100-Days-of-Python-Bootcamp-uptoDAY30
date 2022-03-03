43a9fa5e81ff518fd1f80494129c4d390bb37500
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-03 17:07:30 +0000

    More conversion examples of data types

---
+++ Converting data types2.py
@@ -0,0 +1,14 @@
+a = 123 #here 123 is an integer
+print(type(a))
+
+#int 123 converted to string
+b = str(123)
+print(type(b))
+
+#int 123 converted to float
+c = float(123)
+print(type(c))
+
+print(70 + float("100.5")) #here we are converting the string 100.5 to a float and adding to 70
+
+print(str(70)+ str(100)) #here we are converting 70 and 100 into a the string 70100
