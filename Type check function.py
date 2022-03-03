c280c8cf37416586dde6764df30951caa72c588a
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-03 16:48:04 +0000

    Checking the type of variables

---
+++ Type check function.py
@@ -0,0 +1,8 @@
+num_char = len(input("What was the amount of the entire bill?"))
+# WRONG =   print(Your bill will be  " + num_char + "chahacters")
+
+#TYPE CHECKING
+#EX: 1
+print(type(num_char))
+#EX: 2
+print(type(len(input("What is your bill?"))))
