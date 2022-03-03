9be49e06cd7d3de6ef18241c4c57b7cc624a4613
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-03 16:58:09 +0000

    Converting int to string

---
+++ Converting data types.py
@@ -0,0 +1,6 @@
+num_char = len(input("What is your pet's name"))
+print(type(num_char))
+#CONVERT int to string
+new_num_char = str(num_char)
+
+print("Your pet's name has " + new_num_char + " characters.")
