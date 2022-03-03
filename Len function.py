d6f6b9be0beb725c9efbfdb2e785ea2aebe8d950
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-03 16:45:49 +0000

    More about the len function

---
+++ Len function.py
@@ -0,0 +1,7 @@
+num_char = len(input("What was the amount of the entire bill?")) #if you enter 20, then output is 2
+print(num_char)
+
+
+
+name_char = len(input("Name:")) #if you enter the name kotha, ouput is 5
+print(name_char)
