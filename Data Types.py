40d445f02063d9f3a1a2f448b2895f9c31685eeb
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-03 16:23:54 +0000

    Added strings, integers, floats and booleans to data types

---
+++ Data Types.py
@@ -0,0 +1,19 @@
+#DATA TYPES
+
+#STRINGS (characteristics of a string) this is SUBSCRIPTING=pulling out a number from a string
+print("Hello"[0])
+print("Hello"[4])
+print("123"[2]) #here 123 is a string
+print("123"+"345")#here 123 and 456 are strings
+
+#INTEGERS
+print(123+456) #here 123 & 456 are integers, and are thus added in the result
+
+#FLOATS = floating point number = decimal numbers
+
+print(3.142)
+
+#Booleans
+
+True
+False
