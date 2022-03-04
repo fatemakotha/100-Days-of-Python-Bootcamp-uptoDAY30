fe7675d707e44a8c5d8367d41cb720b77ccd41c9
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-04 08:32:05 +0000

    Using +- -= *= and /=

---
+++ Using += -= *= etc.py
@@ -0,0 +1,11 @@
+x = 4 / 2 #is 2
+x /= 2 #is 2/2 which is 1
+print(x)
+
+score = 0
+#User scores a point 
+#we can use score = score + 1
+#OR:
+score += 1
+print(score) #gives 1
+#We can use += -= *= and /=
