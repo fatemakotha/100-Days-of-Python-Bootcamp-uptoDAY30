3e7d582740c695472d9561fd66d5a29f2048feb3
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-04 07:54:22 +0000

    Working of mathematical operators

---
+++ Mathematical operators.py
@@ -0,0 +1,23 @@
+#MATHEMATICAL OPERATORS
+
+#ADDITION
+add =7 -3
+print(add)
+
+#SUBTRACTION
+sub = 5 + 5
+print(sub)
+
+#MULTIPLICATION
+mul = 3*3
+print(mul)
+
+#DIVISION
+div = 6 / 3 #division always gives out a float, even if the answer is a whole number
+print(div) #output is 2.0(a float)
+print(type(mul))
+
+#RAISE A NUMBER TO IT'S POWER
+
+pow = 2**2
+print(pow)
