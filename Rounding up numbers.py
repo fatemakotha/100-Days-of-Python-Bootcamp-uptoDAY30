aa00f795884c577a37279098fbca78b0b10b2c76
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-04 08:22:46 +0000

    Rounding up, to two decimal places

--- Rounding up numbers.py
+++ Rounding up numbers.py
@@ -5,3 +5,7 @@ print(int(8 / 3)) #gives 2, chops off, instead of rounding up
 #How to round up?
 
 print(round(8 / 3))
+
+#We can also specify the number of digits we want it to round up to
+
+print(round(8 / 3, 2)) #2 means upto two decimal places, gives 2.67 now
