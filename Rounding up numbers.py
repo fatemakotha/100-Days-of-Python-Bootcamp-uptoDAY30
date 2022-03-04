f20c8b5425fb33e0edbe6b541c4a8fe520a0f1e6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-04 08:23:33 +0000

    Extension of rounding up numbers

--- Rounding up numbers.py
+++ Rounding up numbers.py
@@ -9,3 +9,5 @@ print(round(8 / 3))
 #We can also specify the number of digits we want it to round up to
 
 print(round(8 / 3, 2)) #2 means upto two decimal places, gives 2.67 now
+
+print(round(2.66666666666, 2))
