cd1902cd04c7bff810877fa2fccfb98d41f8f747
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-04 08:05:40 +0000

    Manipulating mathematical operators

---
+++ Manipulation of mathematical operators.py
@@ -0,0 +1,11 @@
+print(3 * 3 + 3 / 3 - 3) #  3 / 3 = 1
+                         #  3 * 3 = 9
+                         #  9 + 1 = 10
+                         # 10 - 3 = 7 is how it works
+                         
+#How to we manipulate the above equation to get 3, instead of 7?
+print(3 * (3 + 3) / 3 - 3) # (3 + 3) is now the calculation that will be performed
+# 3 + 3 = 6
+# 3 * 6 = 18
+# 18 / 3 = 6
+# 6 - 3 = 3
