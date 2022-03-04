175e9ccb5ce4832e68a0459a3fe08f4535abb627
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-04 07:53:32 +0000

    Added priority of mathematical operators

---
+++ Priority of mathematical operators.py
@@ -0,0 +1,15 @@
+#PRIORITY OF MATHEMATICAL OPERATORS IF IN A single LINE
+#P E M D A S
+#Parentheses()
+#Exponents**
+#Multiplication *
+#Division /
+#Addition +
+#Subtraction -
+
+# * / MOST TO THE LEFT WILL BE PRIORITISED
+# + - MOST TO THE LEFT WILL BE PRIORITISED
+
+#EXAMPLE
+
+print(3 * 4 + 2 - 3) # 3*4 = 12-3 = 9+2 = 11 is how it works
