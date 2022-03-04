b47ddff58341861437a5342f3eaa1c20526c48ec
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-04 08:41:45 +0000

    Using an F string to print int, float and boolean in one line

---
+++ F strings.py
@@ -0,0 +1,9 @@
+
+score = 0 #an int
+height = 1.8 #a float
+isWinning = True #a Boolean
+
+print("Player with score " + str(score) + " " +str(isWinning) + " with a heigh of " + str(height))
+
+#OR WE CAN USE an F stting
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
