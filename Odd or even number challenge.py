20304f9b2dbd8b5204702ac0fa962686d3cabf48
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-06 17:26:20 +0000

    In if statement, % means divided by

---
+++ Odd or even number challenge.py
@@ -0,0 +1,8 @@
+#ODD OR EVEN NUMBER
+
+a = int(input("Enter a number: "))
+
+if a % 2 == 0: #in if condtition, % refers to divided by 2. and the 0 is the remainder. And when even numbers are divided by 2, the remainder is always 0.
+  print("Even")
+else:
+  print("Odd")
