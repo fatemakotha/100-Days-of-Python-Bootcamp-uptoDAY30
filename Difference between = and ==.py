e71f90926a9105f0021c59546f2311502d017fff
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-06 08:59:59 +0000

    Difference between = and ==

---
+++ Difference between = and ==.py
@@ -0,0 +1,12 @@
+# Difference between = and ==
+a = 10 #we assign value using =
+b = 5
+c = a + b
+print(c) 
+
+#WHEREAS
+a = 30
+if b == 30: #here == represents wehether a statement is true or false
+    print("yes")
+else:
+    print("no")
