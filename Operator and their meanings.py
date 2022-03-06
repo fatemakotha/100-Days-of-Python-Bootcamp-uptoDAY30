8b7d43e2389fcb7d0eaa8bf8ede1ba5327c12199
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-06 09:05:12 +0000

    Introducing operators and their meanings

---
+++ Operator and their meanings.py
@@ -0,0 +1,27 @@
+#OPERATORS and their MEANNINGS:
+# > Greater than
+# < Lesser than
+# >= Greater than or equal to
+# <= Smaller than or equal to
+# == Equal to
+
+a = int(input("Enter value for a:"))
+b = int(input("Enter value for b:"))
+if b == a: #here == represents wehether a statement is true or false
+    print("yes")
+else:
+    print("no")
+    
+a = int(input("Enter value for a:"))
+b = int(input("Enter value for b:"))
+if b >= a: #here == represents wehether a statement is true or false
+    print("yes")
+else:
+    print("no")
+    
+a = int(input("Enter value for a:"))
+b = int(input("Enter value for b:"))
+if b <= a: #here == represents wehether a statement is true or false
+    print("yes")
+else:
+    print("no")
