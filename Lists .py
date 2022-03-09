533c3067cd24281fcccbb14eabc6b5eef5c5a317
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-09 09:50:45 +0000

    Added another example

--- Lists .py
+++ Lists .py
@@ -1,3 +1,4 @@
 #LISTS
 states = ["Newyork", "California", "Texas", "Miami"] #list data structure newyork is the first data, california is second
-print(states[0]) #trying to pull out the first piece of data
+print(states[0]) #trying to pull out the first piece of data, ans= Newyork
+print(states[2)#ans is Texaa
