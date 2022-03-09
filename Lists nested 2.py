05d55409ef52ada9500b612d990c00e15440a8c9
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-09 18:06:59 +0000

    Printing items from nested list

---
+++ Lists nested 2.py
@@ -0,0 +1,12 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+
+
+print(dirty_dozen)
+print(dirty_dozen[1])# print list no 1 which is vegetables
+print(dirty_dozen[0])#prints list no 0 which is fruits
+print(dirty_dozen[1][2])# prints "Tomatoes" as list no 1, item no 2 is tomatoes
+print(dirty_dozen[1][3])# prints "Celery" as list no 1, item no 3 is tomatoes
+print(dirty_dozen[1][1])# # prints "Kale" as list no 1, item no 1 is tomatoes
