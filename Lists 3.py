3411c4cd1e62837fdb14072812607a19129fb7e4
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-09 10:06:41 +0000

    Changing items on a list

---
+++ Lists 3.py
@@ -0,0 +1,12 @@
+#LISTS
+states = ["Newyork", "California", "Texas", "Miami"] #list data structure newyork is the first data, california is second
+print(states[0]) #trying to pull out the first piece of data
+
+print(states[2])#ans is Texas
+
+#INCASE OF COMMING BACKWARDS:
+print(states[-1])#data number 1 from the back is MIAMI
+
+#Change ITEMS ON LIST
+states[1] = "XYZ"
+print(states)
