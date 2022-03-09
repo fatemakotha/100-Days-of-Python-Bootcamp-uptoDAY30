772b3d89e5a575c5c0f3dad95ae5fb976d7c42f1
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-09 10:04:39 +0000

    Going backwards on the list

--- Lists .py
+++ Lists .py
@@ -1,4 +1,5 @@
 #LISTS
 states = ["Newyork", "California", "Texas", "Miami"] #list data structure newyork is the first data, california is second
 print(states[0]) #trying to pull out the first piece of data, ans= Newyork
-print(states[2)#ans is Texaa
+print(states[2)#ans is Texas
+
