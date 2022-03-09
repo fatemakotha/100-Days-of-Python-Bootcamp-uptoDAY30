4c6b42f41719e6193da3a6bc949511300c4fb434
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-09 09:49:37 +0000

    Introducing lists and pulling out one item from a list according to data numbers

---
+++ Lists .py
@@ -0,0 +1,3 @@
+#LISTS
+states = ["Newyork", "California", "Texas", "Miami"] #list data structure newyork is the first data, california is second
+print(states[0]) #trying to pull out the first piece of data
