2dde3a7b7d5a25b22cc871b5e6ec1c17a1b02614
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-23 17:09:38 +0000

    Inside the loop vs outside the loop

---
+++ Loops 4.py
@@ -0,0 +1,7 @@
+#LOOPS
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:#its taking a list of fruits and its assigning variable name 
+    print(fruit)#loops through my list of fruits and prints them one by one
+    print(fruit + " Pie")#loops through my list of fruits and prints them one by one
+print(fruits)#no indentation = outside the loop = follow normal way of printing
+
