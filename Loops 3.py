6c153050e8d0d8971464dfd9f8f11ffd3d8be15a
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-23 17:07:55 +0000

    Better understanding of loops

---
+++ Loops 3.py
@@ -0,0 +1,16 @@
+#LOOPS
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:#its taking a list of fruits and its assigning variable name 
+    print(fruit)#loops through my list of fruits and prints them one by one
+    print(fruit + " Pie")#loops through my list of fruits and prints them one by one
+    print(fruits)#runs as many times as the loop runs
+#prints the following code   
+# Apple
+# Apple Pie
+# ['Apple', 'Peach', 'Pear']
+# Peach
+# Peach Pie
+# ['Apple', 'Peach', 'Pear']
+# Pear
+# Pear Pie
+# ['Apple', 'Peach', 'Pear']
