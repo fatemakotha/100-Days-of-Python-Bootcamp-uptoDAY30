049fce32401c08dff05b36b45b71b41fabafbc89
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-23 17:05:15 +0000

    Printing multiple statements with loop

--- Loops .py
+++ Loops .py
@@ -1,4 +1,7 @@
 #LOOPS
 fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
+for fruit in fruits:#its taking a list of fruits and its assigning variable name "fruit" to each of them
+#the first time it runs fruit = Apple
+#the second time it runs fruit = Peach
+#the third time it runs fruit = Pear
     print(fruit)#loops through my list of fruits and prints them one by one
---
+++ Loops 2.py
@@ -0,0 +1,13 @@
+#LOOPS
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:#its taking a list of fruits and its assigning variable name "fruit" to each of them
+#the first time it runs fruit = Apple
+#the second time it runs fruit = Peach
+#the third time it runs fruit = Pear
+    print(fruit)#loops through my list of fruits and prints them one by one
+    print(fruit + " Pie")#prints Apple
+                         #Apple Pie
+                         #Peach
+                         #Peach Pie
+                         #Pear
+                         #Pear Pie   
