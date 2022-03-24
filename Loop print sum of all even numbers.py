d79cb5e3d5f9e1d1451f645bff800bc7993523d9
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-24 11:49:39 +0000

    Added another method

--- Loop print sum of all even numbers.py
+++ Loop print sum of all even numbers.py
@@ -4,3 +4,11 @@ for number in range(2, 101, 2):
     total += number#this will add every number in the given range, starting from 2 to 100
     print(total)#2+3+4+5+...+100, giving us 2 6 12 ...2550
 print(total)#as the indentation is removed it only prints 2500
+
+
+#OR
+total2 = 0
+for number in range(1,101):
+    if number % 2 == 0:
+     total2 += number
+print(total)#prints 2500
