5f090348b7903cae7a5691ed0111d427332c03c2
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-24 11:26:55 +0000

    Added comment

--- Loop and range function 2.py
+++ Loop and range function 2.py
@@ -3,3 +3,4 @@ total = 0
 for number in range(1, 101):
     total += number#this will add every number in the given range, starting from 0 to 100
     print(total)#1+2+3+4+5+...+100, giving us 1 3 6 ...5050
+print(total)#as th indentation is removed it only prints 5050
