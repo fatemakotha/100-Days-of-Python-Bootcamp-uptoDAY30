2d96565b5bce49d2c15ce798f307093057ced719
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-24 11:16:58 +0000

    Using range in for loop

---
+++ Loop and range function.py
@@ -0,0 +1,9 @@
+#Using for loop with the function range()
+for number in range(1,10):#range is between 1 and 10, not including 10
+    print(number)#prints from 1 2 3 4 5 6 7 8 9)
+    
+for number in range(1,11):#range is between 1 and 10, including 10
+    print(number)#prints from 1 2 3 4 5 6 7 8 9 10)
+    
+for number in range(1,11,3):#range is between 1 and 10, including 10, Here 3 is the step size.
+    print(number)#prints from 1 4 7 10)
