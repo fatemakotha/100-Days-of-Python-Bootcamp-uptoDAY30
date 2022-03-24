7e15ea2fe06624f817e35c777439c0d5022019d6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-24 10:35:14 +0000

    Calculating minimum value using a for loop

---
+++ Lowest score using loop.py
@@ -0,0 +1,11 @@
+student_scores = input("Input a list of student scores ").split()#
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+#input 2 3 1 5 
+lowest_score = student_scores[0]#here lowest score is student_scores value number 0, which is 2
+for number in student_scores:
+    if lowest_score > number:
+       lowest_score = number
+print(lowest_score)
