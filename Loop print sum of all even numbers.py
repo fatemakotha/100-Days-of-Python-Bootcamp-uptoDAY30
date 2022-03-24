5a668836704060ba81010aacc0771c8fd8606ca6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-24 11:28:02 +0000

    Print sum of even numbers and just show the final number in answer

---
+++ Loop print sum of all even numbers.py
@@ -0,0 +1,6 @@
+#Print sum of all even numbers
+total = 0
+for number in range(2, 101, 2):
+    total += number#this will add every number in the given range, starting from 2 to 100
+    print(total)#2+3+4+5+...+100, giving us 2 6 12 ...2550
+print(total)#as the indentation is removed it only prints 2500
