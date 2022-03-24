e5a8e09b030d48a46c011c8751e2ab2b602d8c63
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-03-24 11:21:14 +0000

    Using range function in loop

---
+++ Loop and range function 2.py
@@ -0,0 +1,5 @@
+#using range() in for loop
+total = 0
+for number in range(1, 101):
+    total += number#this will add every number in the given range, starting from 0 to 100
+    print(total)#1+2+3+4+5+...+100, giving us 1 3 6 ...5050
