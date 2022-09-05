4ab66a73e084cb0bd8022b23d7060a3c73192063
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-05 17:23:59 +0000

    Data types

---
+++ 19 Data types and getting hold of first or last alphabet in a string.py
@@ -0,0 +1,30 @@
+#Data types
+
+#Strings
+#gGet a hold of the first character H
+print("Hello" [0])
+#prints:H
+
+#gGet a hold of the last character o
+print("Hello" [4])
+#prints 0
+
+print("123" + "345")
+# prints 123345
+
+#Integer
+print(123 + 345)
+#prints 468
+
+# To write 123456789 as integer write 123_346_678
+print(123_345_456)
+# prints 123345456
+
+print(123_345_456 + 1)
+# prints 123345457
+
+#Float
+print(3.2563)
+
+#Boolean
+#Has only two values True and False
