7bfb53fc4635887d0b1cfa952bf44b29c6190405
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-05 19:40:32 +0000

    Rounding and incrementing

---
+++ 24 Rounding up and incrementing.py
@@ -0,0 +1,24 @@
+print(round(8 / 3)) # rounds the number
+#prinsts 3
+
+print(round(8 / 3, 2)) # rounds the number to 2 decimal places
+#prints 2.67
+
+print(8 // 3) # "//" this is the floor division, which automatically gives an integer answer
+# prints 2
+
+# Continuasly adding inrements to numbers
+result = 4 / 2
+result /= 2 #this means 4/2 then /2 again
+print(result)
+#prints: 1
+
+result = 4 / 2
+result += 2 #this means 4/2 which is 2 and another 2 is added to it
+print(result)
+#prints: 
+
+result = 4 / 2
+result -= 2 #this means 4/2 which is 2 and another 2 is subtracted to it
+print(result)
+#prints: 0
