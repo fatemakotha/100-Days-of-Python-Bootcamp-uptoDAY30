e99cb7414fb61c4e754f6ad9ab0076676c551243
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-06 08:15:10 +0000

    If else and difference between = and ==

---
+++ 29 If else and difference between = and ==.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))#when we use = we are assigning a value to a variable
+
+
+if height == 120: #when we use == we are checking to see if the value of height is equal to 120
+  print("You can ride the rollercoaster!")
+else:
+  print("Sorry")
