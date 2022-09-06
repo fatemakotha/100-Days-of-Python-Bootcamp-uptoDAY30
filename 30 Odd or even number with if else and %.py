92443de238b10bfe08d38b19a713539acbb236c7
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-06 08:19:57 +0000

    Odd or even number with if else and %

---
+++ 30 Odd or even number with if else and %.py
@@ -0,0 +1,6 @@
+number = int(input("Enter a number"))
+
+if number % 2 == 0: #as even numbers can be divided by 2 and have no decimal places in the remeinder
+    print("Even")
+else:
+    print("Odd")
