40be5be7f57ee5d289d6f5ebb8c5c499fa2d0be2
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-07 13:43:16 +0000

    Lists and remove function

---
+++ 43.4 Lists and remove function.py
@@ -0,0 +1,20 @@
+# create a list
+prime_numbers = [2, 3, 5, 7, 9, 11]
+
+# remove 9 from the list
+prime_numbers.remove(9)
+
+
+# Updated prime_numbers List
+print('Updated List: ', prime_numbers)
+
+# Output: Updated List:  [2, 3, 5, 7, 11]
+
+
+
+
+
+states = ["Newyork", "California", "Texas", "Miami"] 
+states.remove("Texas") #['Newyork', 'California', 'Miami'] 
+#***staes.remove(2) DOES NOT WORK
+print(states)
