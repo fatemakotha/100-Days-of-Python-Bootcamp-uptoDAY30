a905d1e93d7d660ae1184229b8e989f32c090c78
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-07 13:57:15 +0000

    Lists and short function

---
+++ 43.8 Lists and sort function.py
@@ -0,0 +1,19 @@
+#***The sort() method sorts the items of a list in ascending or descending order.
+
+prime_numbers = [11, 3, 7, 5, 2]
+
+# sorting the list in ascending order
+prime_numbers.sort()
+
+print(prime_numbers)
+
+# Output: [2, 3, 5, 7, 11]
+
+
+
+
+
+#does not work using index number***
+states = ["Newyork", "California", "Texas", "Miami"] 
+states.sort()
+print(states) #prints: [['California', 'Miami', 'Newyork', 'Texas']
