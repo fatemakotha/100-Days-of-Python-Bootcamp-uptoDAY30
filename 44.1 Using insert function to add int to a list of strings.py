1fad40e03dc2d0cee386e3b31672b195f5a5f473
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-07 14:08:39 +0000

    Using insert function to add int to a list of strings.py

---
+++ 44.1 Using insert function to add int to a list of strings.py
@@ -0,0 +1,17 @@
+states = ["Newyork", "California", "Texas", "Miami"] 
+states.insert(2, 1) # integer inserted at index 2 (3rd position)
+print(states) #prints ['Newyork', 'California', 1, 'Texas', 'Miami']
+
+
+
+
+# create a list of vowels
+vowel = ['a', 'e', 'i', 'u']s
+
+# 'o' is inserted at index 3 (4th position)
+vowel.insert(3, 'o')
+
+
+print('List:', vowel)
+
+# Output: List: ['a', 'e', 'i', 'o', 'u'] *** THIS MAKES THE LIST GO FROM 4 ELEMENTS TO 5 AS IT ADD "0" TO THE EXISTING 4
