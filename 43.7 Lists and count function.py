324a9ebdaf0a2bf30c425a058469c201a613aefd
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-07 13:55:33 +0000

    Lists and count function

---
+++ 43.7 Lists and count function.py
@@ -0,0 +1,20 @@
+#***The reverse() method reverses the elements of the list.
+
+
+# create a list of prime numbers
+prime_numbers = [2, 3, 5, 7]
+
+# reverse the order of list elements
+prime_numbers.reverse()
+
+
+print('Reversed List:', prime_numbers) #prints:: Reversed List: [7, 5, 3, 2]
+
+
+
+
+
+#does not work using index number***
+states = ["Newyork", "California", "Texas", "Miami"] 
+states.reverse()
+print(states) #prints: ['Miami', 'Texas', 'California', 'Newyork']
