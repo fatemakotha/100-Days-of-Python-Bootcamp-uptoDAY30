93a0a9f4c8a1dbb582311209513e1adf3ce16f22
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-07 14:04:31 +0000

    Lists and clear function

---
+++ 49.10 Lists and clear function.py
@@ -0,0 +1,20 @@
+#***The clear() method removes all items from the list.
+
+prime_numbers = [2, 3, 5, 7, 9, 11]
+
+# remove all elements
+prime_numbers.clear()
+
+# Updated prime_numbers List
+print('List after clear():', prime_numbers)
+
+
+# Output: List after clear(): []
+
+
+
+
+#does not work using index number***
+states = ["Newyork", "California", "Texas", "Miami"] 
+states.clear()
+print(states) #prints: []
