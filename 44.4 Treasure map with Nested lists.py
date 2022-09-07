ea90db14408a865f5a6aaf6e071df7c0b12a3823
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-07 17:23:42 +0000

    Added comments to nested list

--- 44.4 Treasure map with Nested lists.py
+++ 44.4 Treasure map with Nested lists.py
@@ -2,7 +2,7 @@
 
 row1 = ["⬜️","⬜️","⬜️"]
 row2 = ["⬜️","⬜️","⬜️"]
-row3 = ["⬜️","⬜️","⬜️*"]
+row3 = ["⬜️","⬜️","⬜️"]
 map = [row1, row2, row3] #THIS IS A NESTED LIST
 print(f"{row1}\n{row2}\n{row3}") #ADDING A LINE BETWEEN EACH ROW
 
@@ -11,17 +11,15 @@ position = input("Where do you want to put the treasure? ") # if you input 23 th
 horizontal = int(position[0]) # we get hold of the 0th item in the string "2" as 0th postition in 23 is 2
 vertical = int(position[1]) # we get hold of the 0th item in the string "3" as 1st position in 23 is 3
 
-print(map[3 -1]) #because in python range srates at 0, in this case range is 0-2
-#OR 
-print(map[vertical -1])#print the row I have selected
 
-selected_row = map[vertical - 1]
-selected_row[horizontal - 1] #selected row at the position horizontal -1
+
+selected_row = map[vertical - 1] #vertical-1= 3-1 = 2, 2nd index in map is row3. Now row3 is selected
+selected_row[horizontal - 1] #horizontal-1 = 2-1 = 1, we get the index one item from row3 which is what we will mark as "X"
 selected_row[horizontal - 1] = "X" #changing that box to x
 # prints:
 # ['⬜️', '⬜️', '⬜️']
 # ['⬜️', '⬜️', '⬜️']
-# ['⬜️', 'X', '⬜️*']
+# ['⬜️', 'X', '⬜️']
     
 
 
