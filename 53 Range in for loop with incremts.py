f0e2caa00e6890257ea44c6568184877d6dcc711
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-08 12:15:34 +0000

    Specifying range in for loop with increments

---
+++ 53 Range in for loop with incremts.py
@@ -0,0 +1,8 @@
+for number in range(1, 10):
+    print(number)#prints 1 2 3 4 5 6 7 8 9 # does not include 10
+
+for number in range(1, 11):
+    print(number)#prints 1 2 3 4 5 6 7 8 9 10 # includes 10
+
+for number in range(1, 11, 2): # the 2 represents increment by 2
+    print(number)#prints 1 3 5 7 9 
