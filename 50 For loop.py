81455c4f055ae5ce7cec7a3af32175253cd89608
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-08 06:19:40 +0000

    Simple for loop

---
+++ 50 For loop.py
@@ -0,0 +1,22 @@
+#A loop allows us to execute the same line of codes, multiple times
+fruits = ["Apple", "Peach", "Pear"]
+#to access all items in this list individually and print it out one by one then we would use a "for" loop.
+#then we give a name to a single term
+for fruit in fruits:
+    print(fruit)#prints the value of each of these fruit variables #we are executing the print statement 3 times
+    #prints
+    # Apple
+    # Peach
+    # Pear
+    #This loop is taking a list of fruits and assigning a variable named fruit to each of them
+    #So the 1st time it runs, fruit = Apple, the 2nd time fruit = Peach and 3rd time fruit = Pear
+    print(fruit + " " + "Pie")
+    #prints:
+    # Apple
+    # Apple Pie
+    # Peach
+    # Peach Pie
+    # Pear
+    # Pear Pie 
+    
+    # ***ALL OF THIS IS EXECUTED AS IT IS INSIDE THE for LOOP
