851892cdd5cbd9ec447b8dd22556c67e392261ed
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-08 12:22:40 +0000

    Adding all numbers in a range using for loop

---
+++ 53.0 Adding all numbers in a range using for loop.py
@@ -0,0 +1,7 @@
+#ADD ALL NUMBER FROM 1 TO 100
+total = 0
+for number in range(1, 101):
+        total += number #addes each number to the previous ones
+print(total) #prints: 5050
+
+# ***if print(total) was indented it would print 1 3 6 10 etc, which means it would show all steps that lead to 5050
