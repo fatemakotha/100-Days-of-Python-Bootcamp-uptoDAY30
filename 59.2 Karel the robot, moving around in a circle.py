1cedb8e987f229722cb9406bcb819457bbb201e0
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-09 16:07:42 +0000

    Karel moves around in a one step circle

---
+++ 59.2 Karel the robot, moving around in a circle.py
@@ -0,0 +1,22 @@
+# PUT THE CODE IN THE LINK BELOW AND TRY IT OUT:
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+turn_right()
+turn_left()
+
+turn_left()
+move()
+
+turn_right()
+move()
+
+turn_right()
+move()
+
+turn_right()
+move()
+#Output: Karel moves around in a one step circle and comes back to original position, facing towards the x-axis in ascending order
