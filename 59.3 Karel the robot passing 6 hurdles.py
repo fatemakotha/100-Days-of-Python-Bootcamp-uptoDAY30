f85036ffa42f6dc8fa11539628b27fb24dd57912
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-09 16:53:57 +0000

    Karel passing 6 hurdles using 2 functions

---
+++ 59.3 Karel the robot passing 6 hurdles.py
@@ -0,0 +1,25 @@
+#GO TO THIS LINK AND PASTE CODE TO SEE HOW IT WORKS:
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+def step_topass_each_hurdle():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+def no_of_steps():
+    step_topass_each_hurdle()
+    step_topass_each_hurdle()
+    step_topass_each_hurdle()
+    step_topass_each_hurdle()
+    step_topass_each_hurdle()
+    step_topass_each_hurdle()
+no_of_steps() 
+#oUTPUT: kAREL passes 6 hurdles 
