7cfba7e9df8ddd5fa978ea9078c02a9755c5ce77
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-10 10:05:32 +0000

    Infinite while loop, while loops keeps running for eternity

---
+++ 62.3 Infinite while loop.py
@@ -0,0 +1,5 @@
+#INFINITE LOOP:
+while 5 > 3:
+   print("HELLO")
+   print("WORLD")#this while loop keeps running for eternity as 5 is always greater than 3
+#Which is why its probably not a good idea
