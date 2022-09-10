80f322cc0fe9da36f9c1012c12083ec1a4f50ee7
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-10 10:08:36 +0000

    Checking for unintended infinite loops

---
+++ 62.4 Checking for unintended infinite loops.py
@@ -0,0 +1,6 @@
+#INFINITE LOOP:
+while 5 > 3:
+   print("HELLO")
+   print("WORLD")#this while loop keeps running for eternity as 5 is always greater than 3
+#Which is why its probably not a good idea
+   print( 5 > 3) #always prints true *** this is how you check for unintended infitine loops
