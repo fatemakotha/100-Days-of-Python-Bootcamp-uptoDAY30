3cf3b179e522c386d123bb0746ae8f08c5e64311
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-11 09:23:45 +0000

    While loop and end function

---
+++ 73.6 While loop to print a number series.py
@@ -0,0 +1,6 @@
+n = 10 #starts at n = 10
+
+while n <= 100: # for n = 10, 10 <= 100 is true #-----1
+    print(n ,end = ",") #prints: 10,
+    n = n+10 #adds 10 to 10 becomes 20, goes back to -----1 and repeats until n = 100
+#prints: 10,20,30,40,50,60,70,80,90,100,
