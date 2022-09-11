5f4dd787e6e83f5d9cb3d027938c4ac8b57c2a02
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-11 08:45:53 +0000

    Breaking a while loop

---
+++ 73.1 Breaking a while loop.py
@@ -0,0 +1,9 @@
+#In Python, we can use the break statement to end a while loop prematurely.
+
+n = 1 #n starts from 1
+
+while n < 5: #n = 1 2 3 4 
+    n = n+1
+    print("Hello Pythonista")
+    if n == 3: #when n = 3 break the loop
+        break #breaks the loop
