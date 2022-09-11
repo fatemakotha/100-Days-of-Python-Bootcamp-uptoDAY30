54a34107e74d50d1accdc661713ae0d61ad7c8c9
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-11 09:20:37 +0000

    Endling print with end function

---
+++ 73.5 End function.py
@@ -0,0 +1,5 @@
+print("Welcome to", end = ',') #ends Welcome to with a comma
+print("GeeksforGeeks", end= ' ')#ends GeeksforGeeks to with a space
+print("hello")
+
+# prints: Welcome to,GeeksforGeeks hello
