1c655ca00557254386baa7d2bd967c306af6c211
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-11 08:43:13 +0000

    How to check if something is or is not in a list

---
+++ 73.0 If or if not in list checker function.py
@@ -0,0 +1,15 @@
+
+list = ['larry', 'curly', 'moe']
+if 'curly' in list:
+    print("yay") #prints: yay
+
+
+
+
+
+
+list = ['larry', 'curly', 'moe']
+if 'curly' not in list:
+    print("boo")
+else:
+    print("curly") #prints: curly
