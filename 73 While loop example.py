dae7e8770e00b590b0ca4ad48918f8d15acc8155
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-11 08:39:44 +0000

    While loop example

---
+++ 73 While loop example.py
@@ -0,0 +1,10 @@
+n = 1 #n starts at 1
+
+while n < 5: #runs loops for n = 1, n = 2, n = 3, n = 4
+    n += 1 # n icreases by 1
+    print("Hello Pythonista") 
+    #prints
+    # Hello Pythonista
+    # Hello Pythonista
+    # Hello Pythonista
+    # Hello Pythonista
