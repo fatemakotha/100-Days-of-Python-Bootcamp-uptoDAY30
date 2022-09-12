a2aa1e6e4f9439406da8892c338b7e592bb975c8
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-12 10:19:15 +0000

    Parameter and arguement - Function with inputs

---
+++ 81.0 Functions with inputs.py
@@ -0,0 +1,22 @@
+import random
+
+def greet():
+    print(f"Hello")
+    print(f"How do you do?")
+    print(f"Isn't the weather nice today? ")
+greet() 
+#prints:
+# Hello
+# How do you do?
+# Isn't the weather nice today? 
+
+def greet_with_name(name): #funcion is greet_with_name() and inside () we create a variable "name"
+    print(f"Hello {name}")
+    print(f"How do you do? {name} ")
+    
+greet_with_name("Kotha") #this assignes "Kotha" to the variable name
+# prints:
+# Hello Kotha
+# How do you do? Kotha 
+
+#*** HERE greet_with_name is the PARAMETER and "Kotha" is the ARGUEMENT
