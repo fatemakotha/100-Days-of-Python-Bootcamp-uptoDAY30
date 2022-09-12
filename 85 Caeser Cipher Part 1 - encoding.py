cc0479a872d9f4e0ee480c834989184dca24e33f
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-12 15:01:37 +0000

    Duplicated the list so that when z is the input it can loop back starting from a again

--- 85 Caeser Cipher Part 1 - encoding.py
+++ 85 Caeser Cipher Part 1 - encoding.py
@@ -1,5 +1,6 @@
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    #we duplicated the list so that when z is the input it can loop back starting from a again
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
