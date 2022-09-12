e97ec956ce76ce3bbd78542f5b24571e18186617
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-12 08:58:06 +0000

    If statement and its negation

---
+++ 74.0 If statement and its negation explained.py
@@ -0,0 +1,29 @@
+
+#Negations:
+#---------2
+number = int(input("Enter 3 or 8: "))
+if not number > 5: #if number is smaller than 6 i.e. 3
+# if not 3 > 5:
+# if not False:
+# if True: #when the value after if is True it prints: print(f"{number} is smaller than 5")
+    print(f"{number} is smaller than 5")
+    #when number = 3 prints 3 is smaller than 5
+else:
+    #if number is greater than 6 i.e. 8
+    # if not 8 > 5:
+    # if not True:
+    # if False: #when the value after if is False it prints: print(f"{number} is greater than 5")
+    print(f"{number} is greater than 5")
+    #when number = 8 prints: 8 is greater than 5
+    
+    
+    
+    
+#--------2
+
+new_number = int(input("Enter 3 or 8: "))
+if new_number > 5: 
+    print(f"{new_number} is greater than 5")
+else:
+    print(f"{new_number} is gsmaller than 5")
+    #when number = 8 prints: 8 is smaller than 5
