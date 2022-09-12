cb1662749352f2eb21b5a07874c0ccb1965b7279
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-12 08:55:33 +0000

    If and if not function

---
+++ 74 If and if not function.py
@@ -0,0 +1,27 @@
+#--------------1 
+today='Raining'
+if not today =='Raining':
+    print('You don’t need an umbrella')
+else:
+    print('You need an umbrella') #prints: You need an umbrella
+    
+#--------------2
+today='sunny'
+if not today=='Raining':
+    print('You don’t need an umbrella')
+else:
+    print('You need an umbrella') #prints: You don"t need an umbrella
+
+#--------------3
+x = 7
+if not x:
+    print("This is True")
+else:
+    print("This is False")#prints: This is False
+
+#--------------4
+x = 465
+if not x:
+    print("This is True")
+else:
+    print("This is False") #prints: This is False
