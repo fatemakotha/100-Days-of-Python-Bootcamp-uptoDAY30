306b4e3a045083131ec58c82d3eb512b3c52dd5a
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-13 11:27:58 +0000

    Adding items to an empty dictionary

---
+++ 91.1 Adding items to Empty Dictionary.py
@@ -0,0 +1,5 @@
+#How to start with an EMPTY Dictionary:
+empty_dictionary = {}
+#Then add things to the dictionary:
+empty_dictionary["Book"] = "Something to read"
+print(empty_dictionary) #prints: {'Book': 'Something to read'}
