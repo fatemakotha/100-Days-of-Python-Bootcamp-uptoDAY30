f8973d358f460feba2d98709149406329ab2c0a6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-13 12:47:15 +0000

    Dictionary with for loop, if and elif

---
+++ 92 Dictionary with for loop, if and elif.py
@@ -0,0 +1,28 @@
+student_scores = {
+  "Hermione": 99,
+  "Ron": 78,
+  "Harry": 81, 
+  "Draco": 74,
+  "Neville": 62,
+} #THIS IS A DICTIONARY
+
+#TODO-1: Create an empty dictionary called student_grades.
+#How to start with an EMPTY Dictionary:
+student_grades = {} #another dictionary
+
+for student in student_scores:
+    score = student_scores[student] #score is a new variable
+    # score = student_scores[Hermione]
+    # score = 99
+    if score > 90: #if 99 > 90
+        student_grades[student] = "Outstanding!" #as 99 > 90, student_grades[Hermione] is set to "Outstanding!"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectations"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "FAIL"
+
+print(student_grades)
+#PRINTS:
+# {'Harry': 'Exceeds Expectations', 'Ron': 'Acceptable', 'Hermione': 'Outstanding!', 'Draco': 'Acceptable', 'Neville': 'FAIL'}  
