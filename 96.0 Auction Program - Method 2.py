3b83d575537b95b51c43572e73043417f43b2b78
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-14 18:06:13 +0000

    Work in progress

--- 96.0 Auction Program - Method 2.py
+++ 96.0 Auction Program - Method 2.py
@@ -6,16 +6,25 @@ while not end:
     name = input("What is your name?: ")
     the_bid= int(input("What is your bid?: $"))
     more_entry = input("yes or no")
-    
-    
+    result = 0
+ 
     if more_entry == "yes":
         thisdict[name] = the_bid #equivalent to += in a list
-        print(thisdict)
+        print(thisdict) #{'kk': 3, 'cutu': 999}
         
     elif more_entry == "no":
         end = True
-        winner = thisdic.max()
-        print("No more entries")
+        max = max(thisdict.values())
+        print(max)
+        
+        
+        
+        
+
+        
+        
+        
+        
         
     else:
         print("FINISHED")
