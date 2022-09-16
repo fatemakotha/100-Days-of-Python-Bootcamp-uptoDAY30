54fc7fffd964c7b3ed8de2268c4f134df594e1a6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-16 16:09:09 +0000

    Format function

---
+++ 99 Format function.py
@@ -0,0 +1,8 @@
+#Functions with OUTPUTS:
+def format_name(f_name, l_name): #f_name and l_name are the PARAMETERS or INOUTS
+    print(f_name.title()) #title function converts the first letter of every wordto uppercase and the rest to lowercase
+    print(l_name.title())
+format_name("angela", "KoThA")
+#prints:
+# Angela
+# Kotha
