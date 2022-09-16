f158b8f7a3f95f18a9cb9e2ffc92e4b5b00a960c
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-16 02:19:03 +0000

    Imported math

--- 98 Calculator - attempt.py
+++ 98 Calculator - attempt.py
@@ -1,3 +1,5 @@
+import math 
+
 first_num = int(input("Enter the 2st number: ")) #input 8 here
 second_num = int(input("Enter the 2nd number: ")) #input 2 here
 operator = input("Chose the operator:\n +, -, * or /")
