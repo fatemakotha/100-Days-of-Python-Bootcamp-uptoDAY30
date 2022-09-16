e1b00b0b09dd6c2d54fb0c4cff0bbb062aefbaf6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-16 02:23:01 +0000

    Fixed bug

--- 98 Calculator - attempt.py
+++ 98 Calculator - attempt.py
@@ -18,7 +18,7 @@ elif operator == "-":
 elif operator == "*":
     ans3 = dic["first_num"] * dic["second_num"]
     print(ans3)
-elif operator == "-":
+elif operator == "/":
     ans4 = math.ceil(dic["first_num"] / dic["second_num"]) #if 1st no is 5 and 2nd no is 2, then when divided, the ans is 2.5 which is not a whole number. Thus we use math.ceil() to round the number up
     print(ans4)
 else:
