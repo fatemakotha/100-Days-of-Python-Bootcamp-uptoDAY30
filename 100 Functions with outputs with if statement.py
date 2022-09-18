1576d81d1108bad406b4a13b8d2540c01a1c9515
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-18 14:45:00 +0000

    Functions with outputs with if statement

---
+++ 100 Functions with outputs with if statement.py
@@ -0,0 +1,9 @@
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return f"You did not input one of the names" #printed when either the first or last name is not given by the user
+    #** In this case the next lines of the code which turns the words to TITLE, are not executed,
+    formatted_f_name = f_name.title()
+    formatted_l_name = l_name.title()
+    return f"{Result: formatted_f_name} {formatted_l_name}" # *** return means this is the end of a function and any code written after is will not run
+    
+print(format_name(input("What is your first name?"), input("What is your last name?")))
