1814caa46e4c7f03f46f6d74c5763d982814f88c
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 16:26:51 +0000

    Quiz 3

---
+++ 119.2 Scope - quiz 3.py
@@ -0,0 +1,15 @@
+What will be printed in the console when the following code is run?
+
+DO NOT run the code, just pretend to be a computer.
+
+def bar():
+    my_variable = 9
+ 
+    if 16 > 9:
+      my_variable = 16
+ 
+    print(my_variable)
+ 
+bar()
+#prints 16
+#Remember that in Python there is no block scope. Inside a if/else/for/while code block is the same as outside it.
