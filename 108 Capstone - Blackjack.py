1a4c4242a12845bcb4ada2953f64575c3ad444c6
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 13:36:59 +0000

    Added multiple if statements to part 3

--- 108 Capstone - Blackjack.py
+++ 108 Capstone - Blackjack.py
@@ -16,7 +16,21 @@ print(f" User cards: {user_cards} and Computer cards: {computer_cards}")
 # prints: User cards: [11, 10] and Computer cards: [4, 10]   
 
 # PART 3
-def calculate_score(cards):
-    score = sum(cards)
-    print(score) #prints: 95 which is addition of all cards in the deck
-calculate_score(cards)
+# PART 3
+def calculate_score(user_cards, computer_cards):
+    user_score = sum(user_cards)
+    computer_score = sum(computer_cards)
+    print(f"USER SCORE: {user_score} and COMPUTER SCORE: {computer_score}")
+    #prints: USER SCORE: 22 and COMPUTER SCORE: 16
+    if 11 in user_cards and 10 in user_cards and user_cards == 2:
+        return 0
+    if 11 in computer_cards and 10 in computer_cards and computer_cards == 2:
+        return 0
+    if 11 in user_cards and user_score > 21:
+        user_cards.remove(11)
+        user_cards.append(1)
+    if 11 in computer_cards and computer_score > 21:
+        computer_cards.remove(11)
+        computer_cards.append(1)
+    print(f"USER cards: {user_cards} and COMPUTER CARDS: {computer_cards}") #prints: USER cards: [11, 1] and COMPUTER CARDS: [6, 10] **The 11 has been changed to 1
+calculate_score(user_cards, computer_cards)
