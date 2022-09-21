afd091baf6ccdf8b75ce8bb15d25d6ffcede74d5
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 13:44:16 +0000

    Added a while loop in calculate_score()

--- 108 Capstone - Blackjack.py
+++ 108 Capstone - Blackjack.py
@@ -16,21 +16,24 @@ print(f" User cards: {user_cards} and Computer cards: {computer_cards}")
 # prints: User cards: [11, 10] and Computer cards: [4, 10]   
 
 # PART 3
-# PART 3
 def calculate_score(user_cards, computer_cards):
     user_score = sum(user_cards)
     computer_score = sum(computer_cards)
+    # PART 4
+    is_game_over = False
     print(f"USER SCORE: {user_score} and COMPUTER SCORE: {computer_score}")
     #prints: USER SCORE: 22 and COMPUTER SCORE: 16
-    if 11 in user_cards and 10 in user_cards and user_cards == 2:
-        return 0
-    if 11 in computer_cards and 10 in computer_cards and computer_cards == 2:
-        return 0
-    if 11 in user_cards and user_score > 21:
-        user_cards.remove(11)
-        user_cards.append(1)
-    if 11 in computer_cards and computer_score > 21:
-        computer_cards.remove(11)
-        computer_cards.append(1)
+    while not is_game_over: 
+        if 11 in user_cards and 10 in user_cards and user_cards == 2:
+            is_game_over = True
+            return 0
+        if 11 in computer_cards and 10 in computer_cards and computer_cards == 2:
+            return 0
+        if 11 in user_cards and user_score > 21:
+            user_cards.remove(11)
+            user_cards.append(1)
+        if 11 in computer_cards and computer_score > 21:
+            computer_cards.remove(11)
+            computer_cards.append(1)
     print(f"USER cards: {user_cards} and COMPUTER CARDS: {computer_cards}") #prints: USER cards: [11, 1] and COMPUTER CARDS: [6, 10] **The 11 has been changed to 1
 calculate_score(user_cards, computer_cards)
