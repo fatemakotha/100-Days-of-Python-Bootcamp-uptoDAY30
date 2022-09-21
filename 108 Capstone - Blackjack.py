18f06ca1fed586dce16de6b3c56731d0d8a22407
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 13:57:53 +0000

    Added an input question in part 3

--- 108 Capstone - Blackjack.py
+++ 108 Capstone - Blackjack.py
@@ -23,17 +23,27 @@ def calculate_score(user_cards, computer_cards):
     is_game_over = False
     print(f"USER SCORE: {user_score} and COMPUTER SCORE: {computer_score}")
     #prints: USER SCORE: 22 and COMPUTER SCORE: 16
-    while not is_game_over: 
-        if 11 in user_cards and 10 in user_cards and user_cards == 2:
-            is_game_over = True
-            return 0
-        if 11 in computer_cards and 10 in computer_cards and computer_cards == 2:
-            return 0
-        if 11 in user_cards and user_score > 21:
-            user_cards.remove(11)
-            user_cards.append(1)
-        if 11 in computer_cards and computer_score > 21:
-            computer_cards.remove(11)
-            computer_cards.append(1)
-    print(f"USER cards: {user_cards} and COMPUTER CARDS: {computer_cards}") #prints: USER cards: [11, 1] and COMPUTER CARDS: [6, 10] **The 11 has been changed to 1
+    def repeat():
+        while not is_game_over: 
+            if 11 in user_cards and 10 in user_cards and user_cards == 2:
+                is_game_over = True
+                return 0
+            if 11 in computer_cards and 10 in computer_cards and computer_cards == 2:
+                return 0
+            if 11 in user_cards and user_score > 21:
+                user_cards.remove(11)
+                user_cards.append(1)
+                is_game_over = True
+            if 11 in computer_cards and computer_score > 21:
+                computer_cards.remove(11)
+                computer_cards.append(1)
+                is_game_over = True
+                
+            draw_another_card = input("Do you you want to draw another card? Type y for yes and n for no: ")
+            if draw_another_card == "n":
+                is_game_over = True
+            elif draw_another_card == "y":
+                user_cards.append(deal_card())
+                repeat()
+   
 calculate_score(user_cards, computer_cards)
