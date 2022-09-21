5137275336e6add4d644c24d61685fc85c35452a
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 15:27:00 +0000

    Scope in for and while loop and if statements

---
+++ 117 Scope - does not work for while and for loops or if statements.py
@@ -0,0 +1,11 @@
+if 3 > 2:
+    a_variable = 10 #This does not count as a FENCE in SCOPE which means it will act Globally
+    
+for _ in range(9):
+    x = 2 #This does not count as a FENCE in SCOPE which means it will act Globally
+    
+at_goal = True
+while not at goal:
+    x = 5 #This does not count as a FENCE in SCOPE which means it will act Globally
+
+
