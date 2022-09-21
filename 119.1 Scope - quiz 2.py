e0329618d297de016105198366046d5fd479fde7
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 16:24:52 +0000

    Quiz 2

---
+++ 119.1 Scope - quiz 2.py
@@ -0,0 +1,12 @@
+# What will be printed in the console when the code is run?
+
+# DO NOT run the code, just pretend to be a computer.
+
+i = 50
+def foo():
+    i = 100
+    return i
+ 
+foo()
+print(i)
+#prints: 50
