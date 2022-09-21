e74f07734fbfc6ca986b04fce4e5ba10c70565b8
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 16:21:19 +0000

    Global constants

---
+++ 119 Global constants.py
@@ -0,0 +1,8 @@
+#GLOBAL CONSTANTS
+PI = 3.14159 #global scope is useful for defining constants
+#*** It is a good idea to name global constants using all uppercase
+URL = "https://www.google.com"
+
+def calc():
+    print(pi)
+calc()
