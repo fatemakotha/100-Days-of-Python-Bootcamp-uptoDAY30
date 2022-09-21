4bd5f9d8c5ce129f8b4ffead5beeedfe524fc93e
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-21 16:23:40 +0000

    Quiz 1

---
+++ 119.0 Scope - quiz 1.py
@@ -0,0 +1,12 @@
+# What will be printed in the console when the following code is run?
+
+# DO NOT run the code, just pretend to be a computer.
+
+def a_function(a_parameter):
+    a_variable = 15
+    return a_parameter
+ 
+a_function(10)
+print(a_variable)
+
+#prints: NameError
