e3d15a23a9dad68ce274220e111baa3d1645a01c
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 18:04:41 +0000

    Added comments

---
+++ 121 Number Guessing game.py
@@ -0,0 +1,11 @@
+#Choosing a random number between 1-100
+
+#Make function to set difficulty
+
+#Let user guess a number
+
+#Function to check user's guess against actual answer
+
+#Track no of turns and reduce by 1 if they get it wrong
+
+#repeating the guessing functionality if they got it wrong
