4de8060046e344ff54f968c777ae37c8b8cf3129
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-22 18:08:23 +0000

    Added print statements

--- 121 Number Guessing game.py
+++ 121 Number Guessing game.py
@@ -1,8 +1,13 @@
 #Choosing a random number between 1-100
-
+from random import randint
+print("Welcome to The Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+answer = randint(1,100)
+print(answer)
 #Make function to set difficulty
 
 #Let user guess a number
+
 
 #Function to check user's guess against actual answer
 
