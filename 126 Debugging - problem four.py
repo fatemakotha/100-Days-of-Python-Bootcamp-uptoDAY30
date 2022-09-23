41f091f3a0cf28af313506fa9bc4b4057f9e9123
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-23 14:19:29 +0000

    Debugging

--- 126 Debugging - problem four.py
+++ 126 Debugging - problem four.py
@@ -3,4 +3,5 @@
 age = int(input("How old are you?"))
 if age > 18:
 # print("You can drive at age {age}.") #no indentation before print statement
-    print("You can drive at age {age}.")
+    print("You can drive at age {age}.") #prints: You can drive at age {age}. This is because we did NOT USE an f-string
+    print(f"You can drive at age {age}.") #prints: You can drive at age 22.
