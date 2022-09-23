021e5144d4e661e3c92bd78d3d949b83607e8627
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-23 14:21:46 +0000

    Debugging

---
+++ 127 Debugging - problem five.py
@@ -0,0 +1,11 @@
+
+#Print is Your Friend
+pages = 0
+word_per_page = 0
+pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: ")) # == means checking a value instead of assigning one
+word_per_page = int(input("Number of words per page: "))
+total_words = pages * word_per_page
+#check for error using print statements(I legit do this ALL THE TIME XD!)
+print(word_per_page)
+print(total_words)
