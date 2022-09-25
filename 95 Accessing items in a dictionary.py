54dfa63f9493ad695c2f3a9a082b5b1b388e2e7b
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-09-25 16:11:14 +0000

    Accessing items from a dic which is inside another dic

---
+++ 95 Accessing items in a dictionary.py
@@ -0,0 +1,12 @@
+#Dictionary inside a dictionary:
+contacts = {
+    "James": {
+        "phone": 54563125,
+        "email": abc@gmail.com,
+    },
+    "Kotha": {
+        "phone": 75465515478,
+        "email": kotha@gmail.com,
+    }
+}
+kotha_email = contacts["Kotha"]["email"]
