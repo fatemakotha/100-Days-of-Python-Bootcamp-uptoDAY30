436760772a06e8b93462b541ef84fd53b438733a
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-03 16:35:00 +0000

    Create a Class with instance attributes

--- 136 Create a Class with instance attributes.py
+++ 136 Create a Class with instance attributes.py
@@ -1,8 +1,17 @@
 # Write a Python program to create a Vehicle class with max_speed and mileage instance attributes.
+
+
+#CLASS name: Vehicle
 class Vehicle:
     def __init__(self, max_speed, mileage):
         self.max_speed = max_speed
         self.mileage = mileage
+#OBJECT name: modelX, modelY and modelZ
+modelX = Vehicle(240, 18) 
+print(modelX.max_speed, modelX.mileage) #prints: 240 18
 
-modelX = Vehicle(240, 18)
+modelY = Vehicle(240, 18)
+print(modelX.max_speed, modelX.mileage) #prints: 240 18
+
+modelZ = Vehicle(240, 18)
 print(modelX.max_speed, modelX.mileage) #prints: 240 18
