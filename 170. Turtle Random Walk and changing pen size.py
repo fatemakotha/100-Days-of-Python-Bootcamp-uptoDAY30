137998af7e230360681151d5da8e8013a1dd7fc0
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-05 09:44:26 +0000

    Turtle Random Walk and changing pen size

--- 170. Turtle Random Walk and changing pen size.py
+++ 170. Turtle Random Walk and changing pen size.py
@@ -6,19 +6,21 @@ cutu = Turtle()
 # print(cutu_the_turtle) #window opens but dissapears
 cutu.shape("turtle") #makes the arrow change into a turtle
 cutu.color("dark cyan")
+cutu.pensize(10) #increases the width of the pen
 
 colours = ["blue", "red", "pink", "black", "purple", "green", "yellow"]
+cutu.forward(100)
 
-def draw_shape(num_sides): #takes in num_sides = no_of_sides which is 3, 4, 5 ,6 ....11
-    interior_angle = 360 / num_sides
-    for _ in range(num_sides):
-        cutu.forward(100)
-        cutu.right(interior_angle)
+directions = [0, 90, 180, 270]
+
+for _ in range(20):
+    cutu.pencolor(random.choice(colours))
+    cutu.forward(30) #moves 30 units
+    cutu.setheading(random.choice(directions)) #changes the left&right to the degrees in the direction list
 
 
-for no_of_sides in range(3, 11):
-    cutu.color(random.choice(colours)) #Turtle draws shapes using random colours***
-    draw_shape(no_of_sides) #no_of_sides: 3, 4, 5 ,6 ....11
+
+
 
 
 
