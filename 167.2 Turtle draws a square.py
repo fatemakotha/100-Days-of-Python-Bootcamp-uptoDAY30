3bbc78c7d8e5987bd02268d314b74f6537e6f680
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-05 06:39:59 +0000

    Turtle draws a square

---
+++ 167.2 Turtle draws a square.py
@@ -0,0 +1,25 @@
+from turtle import Turtle, Screen # imports the Turtle and Screen class that's inside the turtle module
+
+#Create a new Turtle OBJECT named cutu_the_turtle
+cutu = Turtle()
+# print(cutu_the_turtle) #window opens but dissapears
+cutu.shape("turtle") #makes the arrow change into a turtle
+cutu.color("dark cyan")
+
+#How to make the object do certain things/FUNCTIONS
+for _ in range(4):
+    cutu.forward(100) #moves forward by a 100 paces
+    cutu.left(90.0) #turns right by 90 degress
+
+
+
+
+
+
+
+
+
+
+
+screen  = Screen()
+screen.exitonclick() #makes the window stay longer #THIS NEEDS TO BE AT THE END OF THE CODE *
