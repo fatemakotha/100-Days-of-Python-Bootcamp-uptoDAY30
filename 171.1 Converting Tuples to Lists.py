936a6dd1d968158ccb442fe8c611d66f6fdfe8c4
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-06 12:23:53 +0000

    Converting Tuples to Lists

---
+++ 171.1 Converting Tuples to Lists.py
@@ -0,0 +1,12 @@
+
+my_tuple = (1, 3, 4)
+print(my_tuple[0]) #prints: 1
+print(my_tuple[1]) #prints: 3
+print(my_tuple[2]) #prints: 4
+
+print(len(my_tuple))
+
+
+# Converting Tuple to List:
+my_list = list(my_tuple)
+print(my_list) #prints: [1, 3, 4]
