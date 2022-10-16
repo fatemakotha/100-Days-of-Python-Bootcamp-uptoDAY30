d4256bc730c35fbee8be7d70e7c23150044cf8c5
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-16 14:26:05 +0000

    Reading CSV data

---
+++ 227. Reading CSV data.py
@@ -0,0 +1,19 @@
+#*** THE DATA IS INSIDE AN EXC
+
+
+#THIS PROCESS WILL NEED STRIPPING AND CLEANING ETC:
+with open("weather_data.csv") as data_file:
+    data = data_file.readlines()
+    print(data)
+
+#INSTEAD, WE CAN USE THIS:
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file) #comes out as a list that we can loop through **
+    temperature = []
+    for row in data:
+        if row[1] != "temp":
+            new_temp = int(row[1])
+            temperature.append(new_temp)
+    print(temperature)
