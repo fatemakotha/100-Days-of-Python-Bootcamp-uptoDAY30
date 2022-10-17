4530ba0e2d4d21abafd2e4a138e766e7c784bdab
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-17 13:28:36 +0000

    Finding Coordinates in a Gif

---
+++ 228.0 Return MAX of a column in pandas.py
@@ -0,0 +1,3 @@
+#Return MAX value of a series/volumn:
+max_temp = data["temp"].max()
+print(max_temp) #prints: 24
---
+++ 230. US-GAME-Finding Coordinates in a Gif.py
@@ -0,0 +1,18 @@
+import turtle
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.title("US States Game")
+
+image = "blank_states_img - Copy.gif"
+
+screen.addshape(image) #adding a shape to turtle
+turtle.shape(image) #using that shape
+
+#THIS FUNCTION PRINTS COORDINATES OF THE MOUSE POINTER WHEN MAP IS CLICKED: ****
+def get_mouse_click_coor(x, y): #returns x and y coordinates
+    print(x, y)
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop() #keeps our screen open even though code has stopped running
+
+#,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,****
