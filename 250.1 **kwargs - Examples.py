5e2b7e1decfcd0f8148cfe906a784fcb3d74368d
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-21 10:25:18 +0000

    Tkinter-Buttons, Entry and Setting component options

--- 250.1 **kwargs - Examples.py
+++ 250.1 **kwargs - Examples.py
@@ -5,6 +5,7 @@ def bar(spam, eggs, toast='yes please!', ham=0):
  
 bar(1, 2) #PRINTS: 1 2 yes please! 0
 
+#........................................................
 
 #EX 2
 # What is the output of this code?
@@ -12,6 +13,8 @@ def bar(spam, eggs, toast='yes please!', ham=0):
     print(spam, eggs, toast, ham)
  
 bar(toast='nah', spam=4, eggs=2) #PRINTS: 4 2 nah 0
+
+#........................................................
 
 #EX 3:
 # What is the output of the code below?
@@ -21,3 +24,14 @@ def test(*args):
 test(1,2,3,5)
 #PRINTS: (1, 2, 3, 4)
 #args is a tuple, hence it will print the parenthesis ****
+
+#........................................................
+
+#EX 4:
+# What is the output of the code below?
+def all_aboard(a, *args, **kw): 
+    print(a, args, kw)
+ 
+all_aboard(4, 7, 3, 0, x=10, y=64)
+#PRINTS: 4 (7, 3, 0) {"x": 10, "y": 64}
+#4 is passed by position. 7,3,0 are collected into a tuple. x and y are in a keyword dictionary *****
