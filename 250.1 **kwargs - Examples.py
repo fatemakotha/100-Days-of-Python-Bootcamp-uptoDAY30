7a0ec19f903ab1126488408d4af10fa2a83eb739
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-21 09:59:42 +0000

    **kwargs

---
+++ 250.1 **kwargs - Examples.py
@@ -0,0 +1,23 @@
+#EX 1:
+# What is the output of this code?
+def bar(spam, eggs, toast='yes please!', ham=0):
+    print(spam, eggs, toast, ham)
+ 
+bar(1, 2) #PRINTS: 1 2 yes please! 0
+
+
+#EX 2
+# What is the output of this code?
+def bar(spam, eggs, toast='yes please!', ham=0):
+    print(spam, eggs, toast, ham)
+ 
+bar(toast='nah', spam=4, eggs=2) #PRINTS: 4 2 nah 0
+
+#EX 3:
+# What is the output of the code below?
+def test(*args):
+    print(args)
+ 
+test(1,2,3,5)
+#PRINTS: (1, 2, 3, 4)
+#args is a tuple, hence it will print the parenthesis ****
