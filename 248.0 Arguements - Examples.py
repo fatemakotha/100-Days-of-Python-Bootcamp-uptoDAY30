bb4b8b68d29302d695978cc987e0214fe83da376
Fatema Alam Kotha <63168025+fatemakotha@users.noreply.github.com>
2022-10-21 08:01:30 +0000

    Arguments

---
+++ 248.0 Arguements - Examples.py
@@ -0,0 +1,44 @@
+#EXAMPLE: 1
+# What is the output of this code? 
+def foo(a, b=4, c=6): 
+    print(a, b, c)
+ 
+foo(1)
+#PRINTS: 1 4 6
+
+#......................................................
+
+#EXAMPLE 2:
+# What is the output of this code?
+def foo(a, b=4, c=6): 
+    print(a, b, c)
+ 
+foo(4, 9)
+#PRINTS: 4 9 6
+
+#......................................................
+
+#EXAMPLE 3:
+# What is the output of this code?
+def foo(a, b=4, c=6): 
+    print(a, b, c)
+ 
+foo(1, 7, 9)
+#PRINTS: 1 7 9
+
+#.....................................................
+
+#EXAMPLE 4:
+# What is the output of this code?
+def foo(a, b=4, c=6):
+    print(a, b, c)
+ 
+foo(20, c=6)
+#PRINTS: 20 4 6
+
+
+
+
+
+
+
